446e45fb2b6447af9bf55282907d3e5e50470a40
Odilf <odysseas.maheras@gmail.com>
2023-11-28 22:35:52 +0100

    initial commit

---
+++ christmas-tree/src/data.rs
@@ -0,0 +1,76 @@
+use serde::{Deserialize, Serialize};
+use super::YEAR;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DayData {
+    pub input: String,
+}
+
+pub fn get_data(day: u32) -> DayData {
+    cache::get_data(day).unwrap_or_else(|| {
+        println!("Fetching data for day {}", day);
+        fetch_and_cache_data(day, &get_session_token_from_env())
+    })
+}
+
+pub mod cache {
+    use std::{
+        fs::{create_dir_all, File},
+        path::PathBuf,
+    };
+
+    use super::DayData;
+
+    const CACHE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/.cache");
+
+    pub fn file_path(day: u32) -> PathBuf {
+        let dir = PathBuf::from(CACHE_DIR);
+        let file_path = dir.join(format!("day{}.ron", day));
+
+        file_path
+    }
+
+    pub fn get_data(day: u32) -> Option<DayData> {
+        let file = File::open(file_path(day)).ok()?;
+
+        Some(ron::de::from_reader(file).expect("Files should only be generated from this program"))
+    }
+
+    pub fn set_data(day: u32, data: &DayData) -> Result<(), std::io::Error> {
+        create_dir_all(CACHE_DIR)?;
+        let file = File::create(file_path(day))?;
+
+        ron::ser::to_writer(file, data)
+            .expect("There shouldn't be a problem with writing regular data");
+
+        Ok(())
+    }
+}
+
+pub fn fetch_and_cache_data(day: u32, session_token: &str) -> DayData {
+    let data = fetch_data(day, session_token).unwrap();
+    cache::set_data(day, &data).unwrap();
+
+    data
+}
+
+fn fetch_data(day: u32, session_token: &str) -> reqwest::Result<DayData> {
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get(&format!(
+            "https://adventofcode.com/{}/day/{}/input",
+            YEAR, day
+        ))
+        .header(reqwest::header::COOKIE, format!("session={session_token}"))
+        .send()?;
+
+    let input = response.text()?;
+
+    Ok(DayData { input })
+}
+
+fn get_session_token_from_env() -> String {
+    dotenv::dotenv().unwrap();
+    std::env::var("AOC_SESSION_TOKEN").unwrap()
+}
---
+++ christmas-tree/src/lib.rs
@@ -0,0 +1,70 @@
+//! Library for holding all the boilerplate code for the advent of code solutions
+//!
+//! It provides the [christmas_tree::day] macro which picks up the solutions for each part and
+//! runs them based on command line arguments.
+//!
+//! You need to have two functions named `part1` and `part2` in the scope of the macro for it to
+//! work. 
+//!
+//! # Usage
+//!
+//! ```rust
+//! use christmas_tree::day;
+//!
+//! day!(1);
+//!
+//! fn part1(input: &str) -> i32 {
+//!     42
+//! }
+//!
+//! fn part2(input: &str) -> i32 {
+//!    69420
+//! }
+//! ```
+//!
+
+use clap::Parser;
+
+pub mod data;
+
+pub use data::get_data;
+
+const YEAR: usize = 2022;
+
+pub type Part = fn(&str) -> i32;
+pub struct Solution {
+    pub part1: Part,
+    pub part2: Part,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long)]
+    part: Option<u32>,
+}
+
+pub fn run_as_main(solution: Solution, day: u32) {
+    let args = Args::parse();
+
+    match args.part {
+        Some(1) => println!("{}", (solution.part1)(&get_data(day).input)),
+        Some(2) => println!("{}", (solution.part2)(&get_data(day).input)),
+        None => {
+            println!("part 1: {}", (solution.part1)(&get_data(day).input));
+            println!("Part 2: {}", (solution.part2)(&get_data(day).input));
+        }
+        _ => panic!("Part should be either 1 or 2"),
+    }
+}
+
+#[macro_export]
+macro_rules! day {
+    ($day:literal) => {
+        fn main() {
+            $crate::run_as_main($crate::Solution {
+                part1,
+                part2,
+            }, $day);
+        }
+    };
+}
