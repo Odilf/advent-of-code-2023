b7e23ef544b833861a1cff0f78a40919af375cb0
Odilf <odysseas.maheras@gmail.com>
2023-12-03 10:09:49 +0100

    Improved day 3 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,30 +1,59 @@
+use glam::i32::IVec2;
 use std::collections::{HashMap, HashSet};
 
 christmas_tree::day!(3);
 
+#[derive(Debug, Clone, Copy)]
+struct NumberData {
+    value: i32,
+    length: i32,
+    index: i32,
+}
+
+impl NumberData {
+    pub fn start(&self, coords: IVec2) -> IVec2 {
+        coords - IVec2::X * self.index
+    }
+}
+
 fn parse(
     input: &str,
     symbol_predicate: impl Fn(char) -> bool,
-) -> (HashMap<(i32, i32), (i32, i32, i32)>, HashSet<(i32, i32)>) {
-    let mut numbers = HashMap::<(i32, i32), (i32, i32, i32)>::new();
-    let mut symbols = HashSet::<(i32, i32)>::new();
+) -> (HashMap<IVec2, NumberData>, HashSet<IVec2>) {
+    let mut numbers = HashMap::<IVec2, NumberData>::new();
+    let mut symbols = HashSet::new();
 
     for (y, line) in input.lines().enumerate() {
-        let y = y as i32;
         for (x, c) in line.chars().enumerate() {
-            let x = x as i32;
+            let coords = IVec2::new(x as i32, y as i32);
+
             if let Some(digit) = c.to_digit(10) {
                 let digit = digit as i32;
-                if let Some(&(prev, length, _index)) = numbers.get(&(x - 1, y)) {
-                    let number = prev * 10 + digit;
-                    for i in 0..=length {
-                        numbers.insert((x - length + i, y), (number, length + 1, i));
+                let coords_prev = coords - IVec2::X;
+                if let Some(&prev) = numbers.get(&coords_prev) {
+                    let value = prev.value * 10 + digit;
+                    for i in 0..=prev.length {
+                        numbers.insert(
+                            prev.start(coords_prev) + IVec2::X * i,
+                            NumberData {
+                                value,
+                                length: prev.length + 1,
+                                index: i,
+                            },
+                        );
                     }
                 } else {
-                    numbers.insert((x, y), (digit, 1, 0));
+                    numbers.insert(
+                        coords,
+                        NumberData {
+                            value: digit,
+                            length: 1,
+                            index: 0,
+                        },
+                    );
                 }
             } else if symbol_predicate(c) {
-                symbols.insert((x, y));
+                symbols.insert(coords);
             }
         }
     }
@@ -32,61 +61,63 @@ fn parse(
     (numbers, symbols)
 }
 
-fn neighbor_offsets() -> impl Iterator<Item = (i32, i32)> {
+fn get_and_remove(numbers: &mut HashMap<IVec2, NumberData>, coords: IVec2) -> Option<i32> {
+    let number = numbers.remove(&coords)?;
+
+    for i in 0..number.length {
+        if i == number.index {
+            continue;
+        }
+
+        let removed = numbers.remove(&(number.start(coords) + IVec2::X * i));
+        debug_assert!(removed.is_some());
+    }
+
+    Some(number.value)
+}
+
+fn neighbor_offsets() -> impl Iterator<Item = IVec2> {
     (-1i32..=1)
         .flat_map(|dy| (-1i32..=1).map(move |dx| (dx, dy)))
-        .filter(|(dx, dy)| *dx != 0 || *dy != 0)
+        .filter(|&(dx, dy)| dx != 0 || dy != 0)
+        .map(|(dx, dy)| IVec2::new(dx, dy))
 }
 
 fn part1(input: &str) -> i32 {
     let (mut numbers, symbols) = parse(input, |c| c != '.');
 
     let mut count = 0i32;
-    for (x, y) in symbols {
-        for (dx, dy) in neighbor_offsets() {
-            let neighbor = (x + dx, y + dy);
-            if let Some(&(number, length, index)) = numbers.get(&neighbor) {
-                count += number;
-
-                for i in 0..length {
-                    let removed = numbers.remove(&(neighbor.0 - index + i, neighbor.1));
-                    assert!(removed.is_some());
-                }
-            }
+    for coords in symbols {
+        for delta in neighbor_offsets() {
+            count += get_and_remove(&mut numbers, coords + delta).unwrap_or(0);
         }
     }
 
     count
 }
 
+fn get_exacly_two(mut iter: impl Iterator<Item = i32>) -> Option<[i32; 2]> {
+    let first = iter.next()?;
+    let second = iter.next()?;
+    let None = iter.next() else {
+        return None;
+    };
+
+    Some([first, second])
+}
+
 fn part2(input: &str) -> i32 {
     let (mut numbers, symbols) = parse(input, |c| c == '*');
 
     let mut output = 0;
-    'outer: for (x, y) in symbols {
-        let mut total_neighbors = 0;
-        let mut part_numbers = [0; 2];
+    for coords in symbols {
+        let iter =
+            neighbor_offsets().filter_map(|delta| get_and_remove(&mut numbers, delta + coords));
 
-        for (dx, dy) in neighbor_offsets() {
-            let neighbor = (x + dx, y + dy);
-            if let Some(&(number, length, index)) = numbers.get(&neighbor) {
-                total_neighbors += 1;
-
-                if total_neighbors > 2 {
-                    continue 'outer;
-                }
-
-                part_numbers[total_neighbors as usize - 1] = number;
-
-                for i in 0..length {
-                    let removed = numbers.remove(&(neighbor.0 - index + i, neighbor.1));
-                    assert!(removed.is_some());
-                }
-            }
+        if let Some(nums) = get_exacly_two(iter) {
+            let gear_ratio = nums.iter().product::<i32>();
+            output += gear_ratio;
         }
-
-        let gear_ratio = part_numbers[0] * part_numbers[1];
-        output += gear_ratio;
     }
 
     output
